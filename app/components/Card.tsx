5242b5a26ff5d7689fe44a22c3dc4bb4484fe8a4
dannysteinbrecher <danny.steinbrecher@outlook.com>
2025-06-22 00:04:46 +0200

    feat: improved responsive

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type CardProps = {
     imageSrc: string;
     title: string;
+    href: string;
     overlayColor?: string;
     textColor?: string;
 }
@@ -10,32 +12,33 @@ type CardProps = {
 export default function Card({
                                  imageSrc,
                                  title,
+                                 href,
                                  overlayColor = 'bg-black/70',
                                  textColor = 'text-[color:var(--color-neutral)]'
                              }: CardProps
 ) {
     return (
-        <div className="relative w-full aspect-[4/3] max-w-[180px] sm:max-w-[220px] md:max-w-[260px] rounded-xl overflow-hidden shadow-lg">
-
-
-        {/* Hintergrundbild */}
+        <Link
+            href={href}
+            className="group relative w-full aspect-[4/3] max-w-[180px] sm:max-w-[220px] md:max-w-[260px] rounded-xl overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
 
             <Image
                 src={imageSrc}
-                alt="Card Image"
-                className="object-cover"
+                alt={title}
+                className="object-cover "
                 fill
             />
 
             {/* Overlay */}
-            <div className={`absolute inset-0 ${overlayColor}`}/>
+            <div className={`absolute inset-0 ${overlayColor} `}/>
 
-            {/* Zentrierter Text */}
             <div className="absolute inset-0 flex items-center justify-center">
                 <p className={`${textColor} text-center text-lg sm:text-xl md:text-2xl font-semibold px-4`}>
-                {title}
+                    {title}
                 </p>
             </div>
-        </div>
+
+        </Link>
+
     );
 }
