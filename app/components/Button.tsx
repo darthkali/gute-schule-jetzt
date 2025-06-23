42ec025dea1c0b6f794e3768faefb6a13adc4c88
dannysteinbrecher <danny.steinbrecher@outlook.com>
2025-06-23 10:41:38 +0200

    feat: added contact page and make the button also capable to handle submits

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,27 +1,41 @@
 'use client'
 
 import Link from "next/link"
+import { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = {
     text: string
-    href: string
+    href?: string
     bgColor?: string
     textColor?: string
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>; // Für z. B. type="submit"
+
 
 export default function Button({
                                    text,
                                    href,
                                    bgColor = 'bg-orange-500',
                                    textColor = 'text-white',
+                                   ...rest
                                }: ButtonProps) {
-    return (
-        <Link
-            href={href}
-            className={`inline-block px-6 py-3 rounded-full font-medium shadow transition-colors duration-300 hover:brightness-110 ${bgColor} ${textColor} transition-transform duration-300 hover:scale-105`}
 
-        >
+    const className = `inline-block px-6 py-3 rounded-full font-medium shadow transition-colors duration-300 hover:brightness-110 ${bgColor} ${textColor} transition-transform duration-300 hover:scale-105`;
+
+    if (href) {
+        return (
+            <Link
+                href={href}
+                className={className}>
+                {text}
+            </Link>
+        );
+    }
+
+
+    return (
+        <button className={className} {...rest}>
             {text}
-        </Link>
-    )
+        </button>
+    );
 }
+
