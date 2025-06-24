e4fbaa1628c1f048f0393baea6fadc076cd78a38
dannysteinbrecher <danny.steinbrecher@outlook.com>
2025-06-24 09:57:44 +0200

    feat: added dysfunktionale schulsystem page

--- app/(root)/dysfunktionales-schulsystem/page.tsx
+++ app/(root)/dysfunktionales-schulsystem/page.tsx
@@ -1,8 +1,200 @@
 import React from 'react'
+import Image from "next/image";
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <div>
+            <section>
+                <div className={"innerBox"}>
+                    <h1>DYSFUNKTIONALES SCHULSYSTEM PRODUZIERT ZU VIELE VERLIERER</h1>
+                    <h2>Probleme haben immer Ursachen - aus ihrer Analyse entstehen die Lösungsansätze</h2>
+                    <p>Wir alle spüren, dass es so nicht weitergehen kann. Unser Schulsystem produziert zu viele
+                        Verlierer. Bildung ist aber eine wesentliche Grundlage für unsere Gesellschaft und ihre
+                        Erneuerung. Das Wohlergehen von Familien, unserer Demokratie, unserer Wirtschaft und unserer
+                        Zukunft hängt davon ab, dass wir unsere Kinder bestmöglich bilden. Schule ist hier ein
+                        Instrument der Gesellschaft. Wir werden den Zusammenhalt unserer Gesellschaft und die Zuversicht
+                        weiter verlieren, wenn wir nicht gemeinsam in eine Transformation kommen.</p>
+                </div>
+            </section>
+            <section className={"bg-[color:var(--color-secondary)] text-[color:var(--color-neutral)]"}>
+                <div className="innerBox grid grid-cols-1 xl:grid-cols-2 gap-4 mt-4 py-4">
+                    <div>
+                        <h2>Welche Funktionen von Schule liegen im Ungleichgewicht ?</h2>
+                        <p>Schule hat in Deutschland drei Funktionen: Aufbewahrung, Auslese und Bildung. Seit der
+                            Corona- Schulschließung ist klar, die allerwichtigste ist heutzutage die Aufbewahrung. In
+                            einer Situation der Unterversorgung mit Personal und Verpflichtung zum Ganztag steht das
+                            Motto - Hauptsache der Laden läuft, Hauptsache, die Kinder sind in der Schule versorgt - an
+                            erster Stelle. Bildungsqualität muss zurückstehen. Nicht nur PISA Studien belegen, dass
+                            unser Schulsystem zu viele VerliererInnen produziert und dem Wohlergehen der anvertrauten
+                            Kinder und Jugendlichen zuwiderläuft. Die frühe Aufteilung der Schüler nach der 4. Klasse in
+                            höhere und nicht so hohe Bildungsgänge ist international eher einzigartig. In der
+                            Fachliteratur gibt es dazu mehrere Kilometer an Veröffentlichungen, die alle in dieselbe
+                            Richtung weisen: Diese Selektion, bereits in den Grundschulen, macht den Schulalltag für
+                            viele zu einer angstbelasteten Veranstaltung und vergiftet die Atmosphäre an deutschen
+                            Regel-Schulen. „Die Hauptschwäche des deutschen Schulsystems ist, dass es die Schulen aus
+                            der Verantwortung entlässt, sich um schwierige Schüler zu kümmern. Kinder mit
+                            Lernschwierigkeiten werden zu schwierigen, störenden und schließlich gestörten Kindern
+                            gemacht. Das ist in dem Ausmaß beispiellos im Vergleich zu allen anderen von Pisa
+                            untersuchten Ländern,“ (Reinhard Kahl). Akademiker sind fälschlicherweise immer noch die
+                            Leitfiguren des Konzepts der Wissensgesellschaft, das ergibt einen hohen Druck für eine
+                            Gymnasialempfehlung auf Kinder und Familien. Viele Kinder der weiterführenden Schulen
+                            erleben: „Du bist das falsche Kind an der richtigen Schule.“ Gleichzeitig wird das System
+                            der Berufsausbildung geschwächt.</p>
+                    </div>
+
+                    <div>
+                        <h2>Welche weiteren veralteten Strukturen bewirken Dsyfunktionalität ?</h2>
+                        <p>Schule wird in historischer Gewohnheit von oben herab hierarchisch strukturiert und mit
+                            (mangelnden) Ressourcen ausgestattet - und das in der Regel von Menschen, die nicht in ihr
+                            arbeiten. Das Konzept ist aus dem vorletzten Jahrhundert: Man nehme eine Großgruppe Kinder,
+                            ein Zimmer und eine Lehrkraft, die im 45 min Takt wechselnd, anwesend ist. Tafel und Schwamm
+                            dazu. Stundenplan, Hefte und Bücher. Neuerdings auch digitale Tafeln, Handys oder Tablets.
+                            Im Extremfall erleben Kinder schon in der Grundschule im Ganztag bis zu 10 Personalwechsel
+                            täglich. Die Stundenpläne und Sachzwänge erfordern unnachgiebige Fachabfolge im vorgegebenen
+                            Takt. Alle Menschen im System müssen funktionieren und die Strukturen aushalten.</p>
+                    </div>
+
+                    <div>
+                        <h2>Welche Rolle spielen Personalmangel und zunehmende Aufgabenfülle?</h2>
+                        <p>Unser Land lässt seit Jahrzehnten eine genügende Personalplanung vermissen. ErstklässlerInnen
+                            fallen ja nicht vom Himmel. Welcher Betrieb weiß sechs Jahre zuvor die Zahlen? Doch es wird
+                            noch nicht einmal eine Lehrkräftereserve. eingeplant So gehören Unterrichtsausfälle, bereits
+                            zu Schuljahresbeginn seit langem zum Alltag. Unsere Schulleitungen sind durch
+                            Organisationsarbeit zugeschüttet und irgendwann einfach nur noch froh, wenn sie über die
+                            Runden kommen.... Hilferufe werden ignoriert. Viele Lehrkräfte gleichen seit Jahrzehnten
+                            aus, was an strukturellen Defiziten krankt. Manche enden im burn-out, andere retten sich in
+                            Dienst nach Vorschrift. Schule und der Lehrberuf bedeuten in der Realität: Arbeit ohne Ende,
+                            auf allen Ebenen präsent sein (auf Kinderebene, Themen-, Material-, Organisations-,
+                            Kooperations-, Elternebene und Bewertungsverantwortung…) und dabei zu versuchen dem
+                            einzelnen Kind gerecht zu werden. Es sind immer wieder Konflikte zu lösen, oft unter
+                            unmöglichen Bedingungen. Eine Einzellehrkraft, die stundenplanmäßig schon wieder in eine
+                            andere Klasse hetzen muss, hat weder die Zeit, noch die Nerven kindgerecht auf Probleme
+                            einzugehen. Die Folgen? Es fehlt an echter Bildungsqualität und wir verlieren stetig neue
+                            Generationen an Kindern. Jedes Kind kommt außerdem mit unglaublichen individuellen Stärken
+                            zur Welt. Welche Verschaltungsmuster im Gehirn aber stabilisiert oder verworfen werden,
+                            entscheidet sich über die Beziehungen zu anderen Menschen. Wir Menschen haben eine
+                            Sonderstellung in der Natur, nur wir können uns eine Vorstellung machen, wie wir leben
+                            wollen. Ein kleines Kind braucht Erwachsene, die ihm helfen, wie das Leben geht. Wir sind
+                            alle Suchende. Die wichtigste Erkenntnis der Hirnforschung lautet: Das menschliche Gehirn
+                            ist lebenslang weitaus formbarer in seiner inneren Struktur und Organisation, als bisher
+                            gedacht. Im kindlichen Gehirn wird viel mehr an Vernetzungspotenzial zwischen den
+                            Nervenzellen bereitgestellt, als jemals genutzt werden kann. Netzwerke, die aber nicht
+                            gebraucht werden, werden wieder abgebaut. Unser Gehirn ist dabei wie ein wachsender Baum,
+                            der sich im Wachsen strukturiert. Es ist also eine Baustelle und entwickelt sich in einem
+                            nutzungsabhängigem Prozess. In der Auseinandersetzung mit der Welt finden im Gehirn
+                            neuronale Bahnungsprozesse statt. Ist ein Problem gut gelöst, wird das Belohnungssystem in
+                            unserem Gehirn aktiviert und es werden Botenstoffe freigesetzt. Diese
+                            Nerven-Wachstumshormone wirken wie Dünger: die Netzwerke, die ein Kind grade zum Lernen
+                            benutzt hat um das Schwierige hinzubringen, werden dadurch immer stärker z.B.beim Fahrrad
+                            fahren. Diese Bahnungsprozesse gelingen besonders unter emotionaler Beteiligung. Wo man mit
+                            etwas in Beziehung geht. Das Glücksgefühl - der Flow als Folge des Lernprozesses gibt die
+                            Energie für weiteres Lernen - kurz: Was man gemeistert hat, beflügelt! Die Freude am Lernen
+                            ist der Ausdruck der Lebendigkeit eines Kindes</p>
+                    </div>
+
+                    <div>
+                        <h2>Wir haben doch gute Bildungspläne und
+                            Lehrkräfte?</h2>
+                        <p>Natürlich haben wir ziemlich gute Werte und hehre Bildungsziele - dem gesellschaftlichen
+                            Fortschritt nach dem 2. Weltkrieg sei Dank. Die Schule soll mündige und demokratische
+                            Menschen erziehen, fachliche Kompetenz vermitteln, Chancengleichheit, Integration &
+                            Inklusion leisten ..... Es gibt unzählige neue Programme, Medien, Fortbildungen über neue
+                            Lernformen oder Schwerpunkte, die Lehrkräfte umsetzen sollen und natürlich auch wollen, ...
+                            Nur: In laufendem Stress-Betrieb bleibt vieles auf der Strecke. Auch die Schulentwicklung
+                            findet nicht systematisch statt, sondern bleibt abhängig von besonders engagierten
+                            Schulleitungen und Kollegien. Die Unterstützung der Schulverwaltung oder der Landespolitik
+                            bleibt insgesamt ungenügend. Die notwendige Transformation gelingt nur im Glücksfall und an
+                            einzelnen Schulen. In der Fläche herrschen notgedrungen business as usual, Stillstand,
+                            Resignation oder Verschleiß.Zwischen Wirklichkeit und Realität sind riesige Unterschiede.
+                            Wir doktern noch ewig, wenn die Strukturen nicht geändert werden.</p>
+                    </div>
+
+                </div>
+            </section>
+            <section>
+                <div className={"innerBox"}>
+                    <h1>Verlierer</h1>
+                    <div className=" grid grid-cols-1 xl:grid-cols-2 gap-4 mt-4 py-4">
+                        <div>
+                            <h2>Verlierer Nr. 1 DIE KINDER UND FAMILIEN</h2>
+                            <p>Mehr als 25% der GrundschülerInnen erreichen die Leistungsstandards in Deutsch und
+                                Mathematik nicht. Immer mehr Kinder und Jugendliche leiden unter Stress, zeigen
+                                psychologische Auffälligkeiten oder haben sonderpädagogische Bedarfe. Die Chance wird
+                                vertan, Kinder und Jugendliche in ihrer Individualität zu erkennen und anzuerkennen und
+                                ihnen die Möglichkeit zu geben, dabei etwas über ihr eigenes Lernen herauszufinden.
+                                Seelische und körperliche Gesundheit, sowie die Entwicklung ihres Potentials erfahren
+                                nicht die Fürsorge, die unser Landesgesetz verspricht. Das System ist nicht dafür
+                                ausgelegt, Kinder langfristig aufzubauen, ihnen die Freude am Lebenslangen Lernen zu
+                                ermöglichen, sondern in Demotivation zuführen. Hinzu kommen die Benachteiligung oder
+                                Ausgrenzung von Kindern. Chancengerechtigkeit sieht anders aus. Familien erleiden
+                                unnötige Belastungen. Wieviel Bildung, Chancen und Lebensfreude hier zu gewinnen wären.
+                                § 11 Landesverfassung BW 11.11.1953 : "Jeder junge Mensch hat ohne Rücksicht auf
+                                Herkunft oder wirtschaftliche Lage das Recht auf eine seiner Begabung entsprechende
+                                Erziehung und Ausbildung. Das öffentliche Schulwesen ist nach diesem Grundsatz zu
+                                gestalten.“</p>
+                        </div>
+
+                        <div>
+                            <h2>Verlierer Nr. 2 Pädagogisches Personal und Schulleitungen</h2>
+                            <p>In unserem sehr schwerfälligen staatlichen Schulsystem geht tagtäglich viel an Engagement
+                                und an eingesetzten Ressourcen verloren. Zuviel Einzelkämpfertum und überholte
+                                Strukturen behindern die notwendige Transformation. „Echte Bildungsqualität fängt damit
+                                an, dass die, die sie herstellen sollen und wollen, überhaupt „Luft“ dafür haben!“ Haben
+                                sie diese nicht, verlieren sie den Erfolg und die Freude am so wichtigen Beruf. In
+                                Zeiten knapper Fachkräfte ein fataler Teufelskreis. Wieviel tolle Arbeitsplätze,
+                                Bildungserfolg und Arbeitsfreude hier zu gewinnen wären. Und natürlich auch viele junge
+                                Menschen, die Lust bekämen diesen sinnvollen Beruf auszuüben… Link zu überholte
+                                Strukturen : Wenn man die aktuellen Aussagen von Herrn Schleicher betrachtet, dann lässt
+                                er keinen Zweifel daran, dass das Modell des jetzigen Unterrichts ausgedient hat. Seine
+                                Meinung ist, dass wir mit unseren VERA- und PISAVergleichsstudien ausreichend bewiesen
+                                haben, dass unser System nicht zu einer Verbesserung sondern zu einer Verschlechterung
+                                der Situation führt. Es ist ein systemisches Problem.</p>
+                        </div>
+
+                        <div>
+                            <h2>Verlierer Nr. 3 ARBEITSWELT</h2>
+                            <p>Der Arbeitsmarkt in unserer alternden Gesellschaft leidet unter akutem Fachkräftemangel.
+                                Handwerksbetriebe finden zu wenige geeignete Auszubildende. Gleichzeitig fehlen zu
+                                vielen Jugendlichen neben Grund- Kompetenzen in Deutsch und Mathematik auch andere
+                                wichtige Fähigkeiten, wie Teamfähigkeit, kritisches Denken, Kreativität,
+                                Eigenverantwortung oder soziale Umgangsformen. Die Wirtschaft braucht aber, gerade in
+                                Zeiten ungünstiger Demographie, dringend kompetente und tatkräftige MitarbeiterInnen.
+                                Diese fallen nicht vom Himmel, sondern können in unseren Schulen gebildet und gewonnen
+                                werden. „Drei Viertel der Menschen in Deutschland arbeiten – bei einem historischen
+                                Höchststand der Beschäftigtenzahlen – im Dienstleistungsbereich. Mehr als 43,3 Prozent
+                                aller Beschäftigten verrichten wissensbasierte Dienstleistungsarbeit.“ (8.4.2025
+                                https://taz.de/Lotters-Transformator/!vn6081100/) (evt. weglassen?)</p>
+                        </div>
+
+                        <div>
+                            <h2>Verlierer Nr. 4 GESELLSCHAFT</h2>
+                            <p>Dieses Bildungssystem lässt Bildungsqualität und Chancengerechtigkeit vermissen. Dadurch
+                                wird weiterhin soziale Ungleichheit erzeugt. Weder Inklusion noch Integration können
+                                ohne eine wirkliche Transformation gelingen. Schule im Stressbetrieb kann auch keine
+                                Demokratiebildung leben und entlässt zu viele Jugendliche, denen es an grundlegenden
+                                Kompetenzen, Eigenverantwortung oder seelischer Gesundheit fehlt. Unsere Gesellschaft,
+                                die inmitten von vielen Herausforderungen steht, erfährt so keinen Zuwachs an
+                                Zusammenhalt, sondern weitergehende Spaltung. Inzwischen ist die Demokratie in
+                                Bedrängnis, die Menschen verlieren die Zuversicht und die AFD erfährt weiteren
+                                Machtzuwachs. Die Folgekosten die ein dysfunktionales Bildungssystem verursacht, sind
+                                nicht direkt sichtbar, aber immens. Jeder Euro, der in unsere Kinder und die
+                                Transformation in ein
+                                erfolgreiches Bildungssystem investiert wird, ist das Zigfache wert. Die beste Rendite
+                                für unsere Gesellschaft steckt in der Bildung aller unserer Kinder. Kein Grund zum
+                                Feiern: Seit zehn Jahren herrscht Stillstand bei der Inklusion. 5. August 2024:
+                                Deutschland hat sich im vergangenen Jahr eine Rüge der Vereinten Nationen dafür
+                                eingehandelt, dass die schulische Inklusion hierzulande in den vergangenen zehn, 15
+                                Jahren kaum vorangekommen ist. Exemplarisch lohnt ein Blick nach Sachsen-Anhalt, wo der
+                                Anteil von Kindern und Jugendlichen, die an gesonderten „Förderschulen“ unterrichtet
+                                werden, so groß ist wie in keinem anderen Bundesland. https://
+                                www.news4teachers.de/2024/08/kein-grund-zum-feiern
+                                seitzehn-jahren-herrscht-stillstand-bei-der-inklusion-wie-dasbeispiel-sachsen-anhalt-zeigt/</p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 export default Page
