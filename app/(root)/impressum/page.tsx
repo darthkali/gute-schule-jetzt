0d2ed9c7bc1e8c5b37a007cdb8489298f83f8546
darthkali <darthkali@outlook.com>
2025-12-08 17:14:08 +0100

    fix: changed gemeinnützig to ehrenamtliche

--- app/(root)/impressum/page.tsx
+++ app/(root)/impressum/page.tsx
@@ -74,10 +74,10 @@ const Impressum = () => {
               </h2>
               <div className='bg-blue-100 p-4 rounded-xl border-l-4 border-blue-400'>
                 <p className='leading-relaxed'>
-                  <strong>Neue Lernkultur Jetzt!</strong> ist eine gemeinnützige
+                  <strong>Neue Lernkultur Jetzt!</strong> ist eine ehrenamtliche
                   Initiative, die sich für bessere Bildung in Deutschland
                   einsetzt. Diese Initiative verfolgt ausschließlich und
-                  unmittelbar gemeinnützige Zwecke und ist nicht
+                  unmittelbar ehrenamtliche Zwecke und ist nicht
                   gewinnorientiert.
                 </p>
               </div>
